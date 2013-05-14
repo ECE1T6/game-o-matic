abd01107cb61621ac757c0934dfbdbbb359e33b1
rbracken <ryan@ryan-lappy.(none)>
2013-05-14 19:44:29 -0400

    -Optimized shift register interface  Committer: rbracken <ryan@ryan-lappy.(none)>
     On branch master
    	modified:   lodev/printScreen.c

--- lodev/printScreen.c
+++ lodev/printScreen.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <wiringPi.h>
+#include <unistd.h>
 
 int xClock(void){
 	digitalWrite(3,HIGH); //3 = "pin three" on RasPi --> x-"clock" pin
@@ -21,7 +22,7 @@ int outputToScreen(void){
 	digitalWrite(2,LOW);
 }
 
-int flushRegisters(void){ //clears all data from shift registers (but doesn't show this on screen)
+int flushAllRegisters(void){ //clears all data from shift registers (but doesn't show this on screen)
 	int y=0,x=0; //x=cols,y=rows
 	digitalWrite(0,LOW); //0 = "pin zero" on RasPi --> x-"data" pin
 	digitalWrite(1,LOW); //1 = "pin one" on Raspi --> y-"data" pin
@@ -32,9 +33,19 @@ int flushRegisters(void){ //clears all data from shift registers (but doesn't sh
 return;
 }
 
+int flushRowRegisters(void){ //clears all data from shift registers (but doesn't show this on screen)
+	int y=0; //x=cols,y=rows
+	digitalWrite(1,LOW); //1 = "pin one" on Raspi --> y-"data" pin
+	for(y=0;y<=48;y++){
+		yClock();
+	}
+return;
+}
+
+
 int printScreen(bool** matrixPtr){//scans across screen ONE FULL TIME.
 	for(int x=0;x<64;x++){ //making assumption of matrix form matrixPtr[x][y]
-		flushRegisters(); 
+		flushRowRegisters();
 		for(int y=0;y<48;y--){
 			if(matrixPtr[x][y]=="true"){
 				digitalWrite(1,HIGH); //1 = "pin one" on Raspi --> y-"data" pin
@@ -43,9 +54,9 @@ int printScreen(bool** matrixPtr){//scans across screen ONE FULL TIME.
 			yClock();
 		}
 		digitalWrite(0,HIGH); //0 = "pin zero" on RasPi --> x-"data" pin
-		xClock;
-		for(z=x;z>0;z--) xClock; //shifts the data over to make sure the proper column is lit
+		if (x!=0) xClock(); //shifts the data over to make sure the proper column is lit
 		outputToScreen();
+		//usleep(260); // leaves the screen on for a while before the next line is lit = 60fps
 	}
 	
 return;
@@ -53,6 +64,9 @@ return;
 
 
 int main(bool** matrixPtr){//matrixPtr points to a bool 64x48 2-d array. Points containing true interpreted on, false is off.
-printScreen(matrixPtr);
+flushAllRegisters(); 
+while(1){
+	printScreen(matrixPtr);
+}
 return;
 }
