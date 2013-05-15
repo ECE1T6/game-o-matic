f0efa8cd71a35417db11a97c5124a2b4d180d4ae
rbracken <ryan@ryan-lappy.(none)>
2013-05-15 01:50:11 -0400

    Updated to include pthread and printScreen.  Committer: rbracken <ryan@ryan-lappy.(none)>  On branch 8x8_matrix
    	modified:   displayTest.c

--- hidev/displayTest.c
+++ hidev/displayTest.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <wiringPi.h>
+#include <pthread.h>
 //#include <printScreen.h> //custom lodev library
 /*Every other size or position value is based on the following. We might add or remove some as we write the program.*/
 #define ARRAY_HEIGHT  8
@@ -14,14 +16,84 @@
 #define BOTTOM_END  ARRAY_HEIGHT-BOTTOM_MARGIN-1
 #define RIGHT_END  ARRAY_WIDTH-RIGHT_MARGIN-1
 
+//### BEGINNING OF printScreen LIBRARY ####
+pthread_t tid[1];
+
+int xClock(void) {
+	digitalWrite(3, HIGH); //3 = "pin three" on RasPi --> x-"clock" pin
+	digitalWrite(3, LOW);
+	return;
+}
+
+int yClock(void) {
+	digitalWrite(4, HIGH); //4 = "pin four" on RasPi --> y-"clock" pin
+	digitalWrite(4, LOW);
+	return;
+}
+
+int outputToScreen(void) {
+	digitalWrite(2, HIGH); //2 = "pin two" on RasPi --> x- & y-"latch" pin set
+	digitalWrite(2, LOW);
+}
+
+int flushAllRegisters(void) { //clears all data from shift registers (but doesn't show this on screen)
+	int y = 0, x = 0; //x=cols,y=rows
+	digitalWrite(0, LOW); //0 = "pin zero" on RasPi --> x-"data" pin
+	digitalWrite(1, LOW); //1 = "pin one" on Raspi --> y-"data" pin
+	for(x = 0; x <= 8; x++) {
+		yClock();
+		xClock();
+	}
+	return;
+}
+
+int flushRowRegisters(void) { //clears all data from shift registers (but doesn't show this on screen)
+	int y = 0; //x=cols,y=rows
+	digitalWrite(1, LOW); //1 = "pin one" on Raspi --> y-"data" pin
+	for(y = 0; y <= 8; y++){
+		yClock();
+	}
+	return;
+}
+
+
+void* printScreen(bool matrixPtr){//scans across screen ONE FULL TIME.
+	for(int x = 0; x < 8; x++) { //making assumption of matrix form matrixPtr[x][y]
+		flushRowRegisters();
+		for(int y = 0;y < 8; y--) {
+			if (matrixPtr[x][y] == "true") {
+				digitalWrite(1, HIGH); //1 = "pin one" on Raspi --> y-"data" pin
+			}
+			else digitalWrite(1, LOW);
+			yClock();
+		}
+		digitalWrite(0, HIGH); //0 = "pin zero" on RasPi --> x-"data" pin
+		if (x != 0) xClock(); //shifts the data over to make sure the proper column is lit
+		outputToScreen();
+		//usleep(260); // leaves the screen on for a while before the next line is lit = 60fps
+	}
+	
+	return;
+}
+//#### IMPLEMENTATION of printScreen : ###
+int printScreenImplement(bool matrixPtr){//matrixPtr points to a bool 64x48 2-d array. Points containing true interpreted on, false is off.
+	flushAllRegisters(); 
+	while(1) {
+		printScreen(matrixPtr);
+	}
+	return;
+}
+
+//##### END OF printScreen LIBRARY ####
+
 void cleanArray(int x, int y, bool array[ARRAY_HEIGHT][ARRAY_WIDTH]){
 	array[x][y] = false;
-return;
+	return;
 }
 
 void updateArray(int x, int y, bool array[ARRAY_HEIGHT][ARRAY_WIDTH]){
 	array[x][y] = true;
-return;
+	return;
 }
 
 void print_test(bool array[ARRAY_HEIGHT][ARRAY_WIDTH]){
@@ -46,7 +118,7 @@ int main (void){
 	bool array[ARRAY_HEIGHT][ARRAY_WIDTH] = {false};
 	int x=0, y=0;
 /*initialization end*/
-
+	pthread_create(&(tid[1]), NULL, &printScreenImplement(array), NULL);
 	while (1){
 	cleanArray(y,x,array);
 	if (y==8)y=0;
