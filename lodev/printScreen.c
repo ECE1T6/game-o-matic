24108bcc4840426f5a4ca0032a9f8944ecca30d3
Amer Hesson <amer@easytag.net>
2013-05-15 00:31:12 -0300

    Update printScreen.c
    Formatted for better readability. Use this as an example for code styling.

--- lodev/printScreen.c
+++ lodev/printScreen.c
@@ -5,16 +5,16 @@
 #include <wiringPi.h>
 #include <unistd.h>
 
-int xClock(void){
-	digitalWrite(3,HIGH); //3 = "pin three" on RasPi --> x-"clock" pin
-	digitalWrite(3,LOW);
-return;
+int xClock(void) {
+	digitalWrite(3, HIGH); //3 = "pin three" on RasPi --> x-"clock" pin
+	digitalWrite(3, LOW);
+	return;
 }
 
-int yClock(void){
+int yClock(void) {
 	digitalWrite(4,HIGH); //4 = "pin four" on RasPi --> y-"clock" pin
 	digitalWrite(4,LOW);
-return;
+	return;
 }
 
 int outputToScreen(void){
@@ -22,51 +22,51 @@ int outputToScreen(void){
 	digitalWrite(2,LOW);
 }
 
-int flushAllRegisters(void){ //clears all data from shift registers (but doesn't show this on screen)
-	int y=0,x=0; //x=cols,y=rows
-	digitalWrite(0,LOW); //0 = "pin zero" on RasPi --> x-"data" pin
-	digitalWrite(1,LOW); //1 = "pin one" on Raspi --> y-"data" pin
-	for(x=0;x<=8;x++){
+int flushAllRegisters(void) { //clears all data from shift registers (but doesn't show this on screen)
+	int y = 0, x = 0; //x=cols,y=rows
+	digitalWrite(0, LOW); //0 = "pin zero" on RasPi --> x-"data" pin
+	digitalWrite(1, LOW); //1 = "pin one" on Raspi --> y-"data" pin
+	for(x = 0; x <= 8; x++) {
 		yClock();
 		xClock();
 	}
-return;
+	return;
 }
 
-int flushRowRegisters(void){ //clears all data from shift registers (but doesn't show this on screen)
-	int y=0; //x=cols,y=rows
-	digitalWrite(1,LOW); //1 = "pin one" on Raspi --> y-"data" pin
-	for(y=0;y<=8;y++){
+int flushRowRegisters(void) { //clears all data from shift registers (but doesn't show this on screen)
+	int y = 0; //x=cols,y=rows
+	digitalWrite(1, LOW); //1 = "pin one" on Raspi --> y-"data" pin
+	for(y = 0; y <= 8; y++){
 		yClock();
 	}
-return;
+	return;
 }
 
 
 int printScreen(bool** matrixPtr){//scans across screen ONE FULL TIME.
-	for(int x=0;x<8;x++){ //making assumption of matrix form matrixPtr[x][y]
+	for(int x = 0; x < 8; x++) { //making assumption of matrix form matrixPtr[x][y]
 		flushRowRegisters();
-		for(int y=0;y<8;y--){
-			if(matrixPtr[x][y]=="true"){
-				digitalWrite(1,HIGH); //1 = "pin one" on Raspi --> y-"data" pin
+		for(int y = 0;y < 8; y--) {
+			if (matrixPtr[x][y] == "true") {
+				digitalWrite(1, HIGH); //1 = "pin one" on Raspi --> y-"data" pin
 			}
-			else digitalWrite(1,LOW);
+			else digitalWrite(1, LOW);
 			yClock();
 		}
-		digitalWrite(0,HIGH); //0 = "pin zero" on RasPi --> x-"data" pin
-		if (x!=0) xClock(); //shifts the data over to make sure the proper column is lit
+		digitalWrite(0, HIGH); //0 = "pin zero" on RasPi --> x-"data" pin
+		if (x != 0) xClock(); //shifts the data over to make sure the proper column is lit
 		outputToScreen();
 		//usleep(260); // leaves the screen on for a while before the next line is lit = 60fps
 	}
 	
-return;
+	return;
 }
 
 
 int main(bool** matrixPtr){//matrixPtr points to a bool 64x48 2-d array. Points containing true interpreted on, false is off.
-flushAllRegisters(); 
-while(1){
-	printScreen(matrixPtr);
-}
-return;
+	flushAllRegisters(); 
+	while(1) {
+		printScreen(matrixPtr);
+	}
+	return;
 }
