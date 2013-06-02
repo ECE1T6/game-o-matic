68e06f73a47f8f45beab0ca4e069b8d6b19998ab
rbracken <ryan.brackenbury@gmail.com>
2013-06-02 18:49:19 -0400

    Changed directory info + added more user notifications

--- make.sh
+++ make.sh
@@ -3,17 +3,19 @@
 
 echo "Starting Compilation..."
 #hidev libraries
-
+cd hidev
 gcc -o pong pong.c -lm
+echo "Pong Compiled."
 gcc -o displayTestLarge displayTestLarge.c -lwiringPi -std=gnu99
-
+echo "displayTestLarge Compiled."
 #lowdev libraries
 
 cd ../lodev
 
 gcc -o flushScreen flushScreen.c -lwiringPi
+echo "flushScreen Compiled."
 gcc -o printScreen printScreen.c -lwiringPi -std=gnu99
-
+echo "printScreen Compiled."
 echo "...Finished."
 exit 1
 
