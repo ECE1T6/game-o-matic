f4582babe76600f8dc7c5d343168710d7439c877
rbracken <ryan.brackenbury@gmail.com>
2013-06-02 18:49:01 -0400

    Changed directory information

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 #Installs pong to the init.d directory, to be started at boot.
 #NEEDS TO BE RUN WITH SUDO PRIVELEDGES
 
+cd hidev
 sudo mv pong /etc/init.d/
 sudo chmod +x /etc/init.d/pong 
 sudo update-rc.d pong defaults 
