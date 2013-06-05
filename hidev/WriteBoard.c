0daf34946cf9b4ff32baab2686ed45a8b11a5b09
ZadehAtta <atta.h.zadeh@gmail.com>
2013-06-05 17:13:24 -0400

    Complete Overhaul

--- hidev/WriteBoard.c
+++ hidev/WriteBoard.c
@@ -8,235 +8,364 @@
 #include <stdbool.h>//have this for C
 
 
-//add period, comma, semicolon, colon, apostrophe, quote, exclamation, question, asterix, hashtag, etc.
-bool board[48][64] = {false};
+const int CHRCX = 5;
+const int CHRCY = 7;
+const int BOARDX = 64;
+const int BOARDY = 48;
+bool board[BOARDY][BOARDX] = {false};
 
 
-bool** makeArray(int arraySizeX, int arraySizeY) {  
-	bool** theArray;  
-	theArray = (bool**) malloc(arraySizeX*sizeof(bool*));  
-	for (int i = 0; i < arraySizeX; i++){
-		theArray[i] = (bool*) malloc(arraySizeY*sizeof(bool)); 
+typedef struct {
+	bool** canvas;
+
+	int x;
+	int y;
+
+	int width;
+	int height;
+
+}drawing;
+
+bool** makeArray(int width, int height) { 
+	width*=30;// I'm sick of this shit
+	height*=30;
+
+	bool** a;  
+	a = (bool**) malloc(width*sizeof(bool*));  
+	for (int i = 0; i < width; i++)  
+		a[i] = (bool*) malloc(height*sizeof(bool));  
+	return a;  
+}
+drawing* addDrawing(drawing* d1, drawing* d2){
+	drawing* finalD = (drawing*)malloc(sizeof(drawing));
+
+	finalD->x = d1->x + d2->x;
+	finalD->y = d1->y;
+
+	finalD->width = d1->width + d2->width;
+	finalD->height = d1->height;
+
+	finalD->canvas = makeArray(finalD->width, finalD->height);
+
+
+	for(int i = 0; i < d1->height; i++){
+		for(int j = 0; j < d1->width; j++){
+			finalD->canvas[i][j]= d1->canvas[i][j];
+		}			
 	}
-	return theArray;  
-}   
-/*
-x1,y1: top left pixel
-x2,y2: bottom right pixel
-xNew,yNew: new top left pixel location to be translated
-*/
-void translateBox(int x1, int y1, int x2, int y2, int xNew, int yNew){
-	bool** temp = makeArray((x2-x1)*4 , (y2-y1)*4);
-
-	for(int i=0;i<(y2-y1);i++){
-		for(int j=0;j<(x2-x1);j++){
-			temp[i][j]=board[y1+i][x1+j];
-		}
-	}
-	
-	for(int i = 0; i < (x2-x1) ; i++){//place back into board
-		for(int j = 0; j < (y2-y1); j++){
-			board[yNew+j][xNew+i]= temp [j][i];
+	for(int i = 0; i < d1->height; i++){
+		for(int j = d1->width; j < finalD->width; j++){
+			finalD->canvas[i][j]= d2->canvas[i][j-d1->width];
 		}			
 	}
 
-	free(temp);
-}
-/*
-rotates 90degrees counter clockwise
-*/
-void rotateCounterClockwise(int x1,int y1, int x2, int y2){
-	bool** temp;
 
-	if((x2-x1)>(y2-y1)){
-		temp = makeArray((x2-x1)*4,(x2-x1)*4);
+	return finalD;
+}
+void insertDrawing(drawing* d){
+	for(int i = 0; i < d->height; i++){
+		for(int j = 0; j < d->width; j++){
+			board[i+d->y][j+d->x]= d->canvas[i][j];
+		}			
+	}
+}
+
+void translateHrz(drawing* d, int factor){
+	d->x += factor;
+}
+void translateVrt(drawing* d, int factor){
+	d->y += factor;
+}
+
+void mirrorHrz(drawing* d){
+	bool temp;
+	for(int i = 0; i < (d->width/2); i++){
+		for(int j = 0; j < d->height; j++){
+			temp = d->canvas[j][i];
+			d->canvas[j][i] = d->canvas[j][d->width-i-1];
+			d->canvas[j][d->width-i-1] = temp;
+		}
+	}
+}
+void mirrorVrt(drawing* d){//top goes to bottom, bottom goes to top
+	bool temp;
+	for(int i = 0; i <= (d->height/2); i++){
+		for(int j = 0; j<d->width; j++){
+			temp = d->canvas[i][j];
+			d->canvas[i][j] = d->canvas[d->height-i-1][j];
+			d->canvas[d->height-i-1][j] = temp;
+		}
+	}
+}
+void rotateCw(drawing* d){
+	int max = (d->height>d->width ? d->height:d->width);
+
+	bool** temp = makeArray(d->height, d->width);//switches width and height when mallocing
+
+	for(int i = 0; i < max; i++){
+		for(int j = 0; j < max; j++){
+			temp[j][i]=d->canvas[i][j];
+		}
+	}
+
+	bool** clone = d->canvas;
+	d->canvas = temp;
+	free(clone);
+
+	int t = d->width;
+	d->width = d->height;
+	d->height = t;
+
+	mirrorHrz(d);
+}
+void rotateCcw(drawing* d){
+	int max = (d->height>d->width ? d->height:d->width);
+
+	bool** temp = makeArray(d->height, d->width);//switches width and height when mallocing
+
+	for(int i = 0; i < max; i++){
+		for(int j = 0; j < max; j++){
+			temp[j][i]=d->canvas[i][j];
+		}
+	}
+
+	bool** clone = d->canvas;
+	d->canvas = temp;
+	free(clone);
+
+	int t = d->width;
+	d->width = d->height;
+	d->height = t;
+
+	mirrorVrt(d);
+}
+void scaleHrz(drawing* d, int factor){
+	d->width *= factor;
+
+	bool** temp = makeArray(d->width,d->height );
+
+	for(int i=0;i<d->height;i++){
+		for(int j=0;j<d->width;j++){
+			temp[i][factor*j]=d->canvas[i][j];
+		}
+	}
+
+	for(int i=0;i<d->height;i++){
+		for(int j=0;j<d->width*factor;j++){
+			temp[i][j]=temp[i][(j/factor)*factor];
+		}
+	}
+
+	bool** clone = d->canvas;
+	d->canvas = temp;
+	free(clone);
+}
+void scaleVrt(drawing* d, int factor){
+	d->height *= factor;
+
+	bool** temp = makeArray(d->width, d->height);
+
+	for(int i=0;i<d->height;i++){
+		for(int j=0;j<d->width;j++){
+			temp[factor*i][j] = d->canvas[i][j];
+		}
+	}
+
+	for(int i=0;i<d->height*factor;i++){
+		for(int j=0;j<d->width;j++){
+			temp[i][j]=temp[(i/factor)*factor][j];
+		}
+	}
+
+	bool** clone = d->canvas;
+	d->canvas = temp;
+	free(clone);
+}
+void shearHrz(drawing* d, int factor){
+	
+
+	bool** temp = makeArray(d->width + abs(factor)*(d->height-1), d->height);
+
+	if (factor > 0){
+		for(int i = 0; i < d->height; i++){
+			for(int j = 0; j < d->width; j++){
+				temp[i][j+((d->height-i-1)*factor)] = d->canvas[i][j];
+			}
+		}
 	}
 	else{
-		temp = makeArray((y2-y1)*4,(y2-y1)*4);
-	}
-
-	for(int i=0;i<(y2-y1);i++){//Stretch
-		for(int j=0;j<(x2-x1);j++){
-			temp[i][j]=board[y1+i][x1+j];
+		factor *= -1;
+		for(int i = 0; i < d->height; i++){
+			for(int j = 0; j < d->width; j++){
+				temp[i][j+(i*factor)] = d->canvas[i][j];
+			}
 		}
 	}
+	bool** clone = d->canvas;
+	d->canvas = temp;
+	free(clone);
 
-	for(int i = 0; i < (x2-x1) ; i++){//place back into board
-		for(int j = 0; j < (y2-y1); j++){
-			board[y1+j][x1+i]= temp [i][j];
-		}			
-	}
-	free(temp);
+	d->width += factor*(d->height-1);
 }
-void skewHorizontal(int x1, int y1, int x2, int y2, int factor){
-}
-void skewVertical(int x1, int y1, int x2, int y2, int factor){
-}
-void stretchHorizontal(int x1, int y1, int x2, int y2, int scale){
-	bool** temp = makeArray((x2-x1)*(scale+1),(y2-y1)*(scale+1));
+void shearVrt(drawing* d, int factor){
+	bool** temp = makeArray(d->width , d->height+ abs(factor)*(d->width-1));
 
-	for(int i=0;i<(y2-y1);i++){//Stretch
-		for(int j=0;j<(x2-x1);j++){
-			temp[i][scale*j]=board[y1+i][x1+j];
+	if (factor > 0){
+		for(int i = 0; i < d->width; i++){
+			for(int j = 0; j < d->height; j++){
+				temp[j+((d->width-i-1)*factor)][i] = d->canvas[j][i];
+			}
 		}
 	}
-
-	for(int i=0;i<(y2-y1);i++){//fill
-		for(int j=0;j<(x2-x1)*scale;j++){
-			temp[i][j]=temp[i][(j/scale)*scale];
+	else{
+		factor *= -1;
+		for(int i = 0; i < d->width; i++){
+			for(int j = 0; j < d->height; j++){
+				temp[j+(i*factor)][i] = d->canvas[j][i];
+			}
 		}
 	}
+	bool** clone = d->canvas;
+	d->canvas = temp;
+	free(clone);
 
-	for(int i = 0; i < (x2-x1)*scale ; i++){//place back into board
-		for(int j = 0; j < (y2-y1); j++){
-			board[y1+j][x1+i]= temp [j][i];
-		}			
-	}
-	free(temp);
-}
-void stretchVertical(int x1, int y1, int x2, int y2, int scale){
-	bool** temp = makeArray((x2-x1)*(scale+1),(y2-y1)*(scale+1));
-
-	for(int i=0;i<(x2-x1);i++){
-		for(int j=0;j<(y2-y1);j++){
-			temp[scale*j][i]=board[y1+j][x1+i];
-		}
-	}
-
-	for(int i=0;i<(x2-x1);i++){
-		for(int j=0;j<(y2-y1)*scale;j++){
-			temp[j][i]=temp[(j/scale)*scale][i];
-		}
-	}
-
-	for(int i = 0; i < (x2-x1) ; i++){
-		for(int j = 0; j < (y2-y1)*scale; j++){
-			board[y1+j][x1+i]= temp [j][i];
-		}			
-	}
-	free(temp);
+	d->height += factor*(d->width-1);
 }
 
-void replacePixels_number( char c, int x, int y){
-	for(int i = 0; i < 7; i++){
-		for(int j = 0; j < 5; j++){
-			board[i+y][j+x]= CHAR_NUM[c][i][j];
+void drawChrc_numb(drawing* d, char c,int blank){
+	for(int i = 0; i < CHRCY; i++){
+		for(int j = 0; j < CHRCX; j++){
+			d-> canvas[i][j+blank]= CHAR_NUM[c][i][j];
 		}			
-	}//replaces necessary pixels one by one.
-}
-void replacePixels_upperCase( char c, int x, int y){
-	for(int i = 0; i < 7; i++){
-		for(int j = 0; j < 5; j++){
-			board[i+y][j+x]= CHAR_UPPER[c][i][j];
-		}			
-	}//replaces necessary pixels one by one.
-}
-void replacePixels_lowerCase( char c, int x,int y){
-	for(int i = 0; i < 7; i++){
-		for(int j = 0; j < 5; j++){
-			board[i+y][j+x]= CHAR_LOWER[c][i][j];
-		}			
-	}//replaces necessary pixels one by one.
-}
-void replacePixels_space(int x, int y){
-	for(int i = 0; i < 7; i++){
-		for(int j = 0; j < 5; j++){
-			board[i+y][j+x]= 0;
-		}			
-	}//replaces necessary pixels one by one.
-}
-/*
-NOTE: currently only works for numbers and capital letters, if need be we can add more characters to it.
-
-char c : the character to be drawn
-x, y: the location of the upper left corner of the 5x7 array inside the big 64x48 array
-note x and y start from 0
-*/
-
-bool drawChar( char c, int x, int y,int scale){//replace 48 and 64 with a CONST when you feel like it
-
-
-	if(x>(64-5*scale) || x<0 || y>(48-7*scale) || y<0){
-		return 0;// false if out of bounds
 	}
+}
+void drawChrc_ucas(drawing* d, char c,int blank){
+	for(int i = 0; i < CHRCY; i++){
+		for(int j = 0; j < CHRCX; j++){
+			d-> canvas[i][j+blank]= CHAR_UPPER[c][i][j];
+		}			
+	}
+}
+void drawChrc_lcas(drawing* d, char c,int blank){
+	for(int i = 0; i < CHRCY; i++){
+		for(int j = 0; j < CHRCX; j++){
+			d-> canvas[i][j+blank]= CHAR_LOWER[c][i][j];
+		}			
+	}
+}
+void drawChrc_spce(drawing* d, int blank){
+	for(int i = 0; i < CHRCY; i++){
+		for(int j = 0; j < CHRCX; j++){
+			d-> canvas[i][j+blank]= false;
+		}			
+	}
+}
 
-	else if(c>='0' && c<='9'){ //Call replacePixels function for numbers only
-		replacePixels_number( c-48, x, y);
+void drawChrc(drawing* d, char c, int boardX, int boardY){
+	d->canvas = makeArray(CHRCX, CHRCY);
+
+	d->x = boardX;
+	d->y = boardY;
+
+	d->width = CHRCX;
+	d->height = CHRCY;
+
+	if(c>='0' && c<='9'){
+		drawChrc_numb(d, c-48, 0);
 	}
-	else if(c>='A' && c<='Z'){ //Call replacePixels function for capital letters only
-		replacePixels_upperCase( c-65 , x, y);
+	else if(c>='A' && c<='Z'){
+		drawChrc_ucas(d, c-65, 0);
 	}
-	else if(c>='a' && c<='z'){ //Call replacePixels function for capital letters only
-		replacePixels_lowerCase( c-97 , x, y);
+	else if(c>='a' && c<='z'){
+		drawChrc_lcas(d, c-97, 0);
 	}
 	else if(c==' '){
-		replacePixels_space(x, y);
+		drawChrc_spce(d, 0);
 	}
-	stretchHorizontal(x,y,x+6,y+8,scale);
-	stretchVertical(x,y,x+6*scale,y+8,scale);
-
-
-	return 1;
 }
-bool drawString(char* s,int length, int x, int y, int scale){//length is length of string (DEAL WITH IT!!!)
-	if (length <= 0)
-	{
-		length = strlen(s);
-	}
-	for(int i=0; i<length; i++){//for loop is... I'm not gonna comment it's kinda obvious actually
-		if(drawChar(s[i],x+6*i*scale,y,scale)){
-			/*IMPORTANT NOTE: the problem with this implementation is it
-			returns the zero after the program has started executing, could be a problem or could be an asset
-			*/
+void drawString(drawing* d, char* s, int boardX, int boardY){
+	int length = strlen(s);
+	char c;
+
+	d->canvas = makeArray(CHRCX,(CHRCY+1)*length);
+
+	d->x = boardX;
+	d->y = boardY;
+
+	d->width = (CHRCX+1)*length;
+	d->height = CHRCY;
+	for(int i=0; i<length; i++){
+		c = s[i];
+
+		if(c>='0' && c<='9'){
+			drawChrc_numb(d, c-48, i*(CHRCX+1));
 		}
-		else{
-			return 0;
+		else if(c>='A' && c<='Z'){
+			drawChrc_ucas(d, c-65, i*(CHRCX+1));
+		}
+		else if(c>='a' && c<='z'){
+			drawChrc_lcas(d, c-97, i*(CHRCX+1));
+		}
+		else if(c==' '){
+			drawChrc_spce(d, i*(CHRCX+1));
 		}
 	}
-	return 1;
 }
 
-
-void printTest(){
-	for(int i = 0; i < 48; i++){
-		for(int j = 0; j < 64; j++){
-			if(board[i][j] == true){
-				printf("#", board[i][j]);
+void testPrint(drawing* d){
+	for(int i = 0; i < d->height; i++){
+		for(int j = 0; j < d->width; j++){
+			if(d->canvas[i][j] == true){
+				printf("#");
 			}
 			else{
-				printf(".", board[i][j]);
+				printf(".");
+			}
+		}
+		printf("\n");
+	}
+	printf("\n");
+	printf("height:%d\n",d->height);
+	printf("width :%d\n",d->width);
+	printf("x     :%d\n",d->x);
+	printf("y     :%d\n",d->y);
+}
+
+void testPrintBoard(){
+	for(int i = 0; i < BOARDY; i++){
+		for(int j = 0; j < BOARDX; j++){
+			if(board[i][j] == true){
+				printf("#");
+			}
+			else{
+				printf(".");
 			}
 		}
 		printf("\n");
 	}
 }
 
-int main()//For testing Purposes only
+int main()
 {
-	bool test;
+	printf("I'm alive!\n");
+	drawing* d = (drawing*)malloc(sizeof(drawing));
 
-	//test = drawString("abc",10,0,0,1);
+	d->canvas = makeArray(CHRCX, CHRCY);
+	drawString(d,"ABC",3,5);
 
-	//stretchVertical(0,0,6*3,8,2);
+	rotateCw(d);
 
-	//translateBox(0,0,64,8,10,10);
+	testPrint(d);
+
+	printf("\n\n\n");
+	testPrintBoard();
+
+	insertDrawing(d);
+
+	printf("\n\n\n");
+	testPrintBoard();
 
 
-
-	
-	test = drawString("The Quick",-1,0,0,1);
-	test = drawString("Brown fox",-1,0,8,1);
-	test = drawString("Jumped",-1,0,16,1);
-	test = drawString("Over the",-1,0,24,1);
-	test = drawString("Lazy dog",-1,0,32,1);
-	
-	//rotateCounterClockwise(0,0,12,8);
-
-	/*for(int i='A';i<='Z';i++){
-	test = drawChar(i,(i-'A')*6 %60 ,(i-'A')/10 *8,1);
-	}*/
-
-	printTest();
 	//system("Pause");//needed for C++
 	return 0;
 }
