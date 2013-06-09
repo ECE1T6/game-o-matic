13b4b7f78f5efdc07400c07f8d8b653b9d28d557
rbracken <ryan.brackenbury@gmail.com>
2013-06-09 11:42:24 -0400

    Fixed to use malloc Includes pin declarations

--- lodev/printScreen.c
+++ lodev/printScreen.c
@@ -1,88 +1,104 @@
-//printScreen.c -- prints all required pixels onto a 64x48 screen. Scans in cols from left/top to right/bottom.
+//printScreen.c -- prints/mux's all required pixels on an LED screen. Scans in rows from top to bottom.
 
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdlib.h>
 #include <wiringPi.h>
 #include <unistd.h>
+#include <stdbool.h>
 #include <pthread.h>
 
-void xClock(void) {
-	digitalWrite(3, HIGH); //3 = "pin three" on RasPi --> x-"clock" pin
-	digitalWrite(3, LOW);
+//#### PIN and CONST. DECLARATIONS ####
+#define	COL 0
+#define ROW 1
+#define LATCH 2
+#define COLCLK 3
+#define ROWCLK 4
+#define ARRAY_HEIGHT  48 //change these to the matrix size
+#define ARRAY_WIDTH  64
+
+void yClock(void) {
+	digitalWrite(COLCLK, HIGH);
+	digitalWrite(COLCLK, LOW);
 	return;
 }
 
-void yClock(void) {
-	digitalWrite(4, HIGH); //4 = "pin four" on RasPi --> y-"clock" pin
-	digitalWrite(4, LOW);
+void xClock(void) {
+	digitalWrite(ROWCLK, HIGH);
+	digitalWrite(ROWCLK, LOW);
 	return;
 }
 
 void outputToScreen(void) {
-	digitalWrite(2, HIGH); //2 = "pin two" on RasPi --> x- & y-"latch" pin set
-	digitalWrite(2, LOW);
+	digitalWrite(LATCH, HIGH); 
+	digitalWrite(LATCH, LOW);
+	return;
 }
 
-void flushAllRegisters(void) { //clears all data from shift registers (but doesn't show this on screen)
-	int y = 0, x = 0; //x=cols,y=rows
-	digitalWrite(0, LOW); //0 = "pin zero" on RasPi --> x-"data" pin
-	digitalWrite(1, LOW); //1 = "pin one" on Raspi --> y-"data" pin
-	for(x = 0; x <= 64; x++) {
+//clears all data from shift registers (but doesn't show this on screen)
+void flushAllRegisters(void) { 	
+  int y = 0, x = 0; //x=#cols,y=#rows
+	digitalWrite(COL, LOW); 
+	digitalWrite(ROW, LOW);
+  //assuming array is strictly as wide or wider than tall
+	for(x = 0; x <= ARRAY_WIDTH; x++) { 
 		yClock();
 		xClock();
 	}
 	return;
 }
 
-void flushRowRegisters(void) { //empties row registers (doesn't show on screen) -- typically not needed
-	int y = 0; //x=cols,y=rows
-	digitalWrite(1, LOW); //1 = "pin one" on Raspi --> y-"data" pin
-	for(y = 0; y <= 64; y++){
-		yClock();
+//clears all data from shift registers (but doesn't show this on screen)
+void flushRowRegisters(void) { 
+	int x = 0; //x=#cols,y=#rows
+	digitalWrite(ROW, LOW); //1 = "pin one" on Raspi --> x-"data" pin
+	for(x = 0; x <= ARRAY_WIDTH; x++){
+		xClock();
 	}
 	return;
 }
 
-
-void printScreen(bool matrixPtr[48][64]){//scans downward, across screen ONE FULL TIME.
-	for(int x = 47; x >= 0; x-=8) { //making assumption of matrix form matrixPtr[x][y]
-		for(int y = 63;y >= 0; y--){
-			digitalWrite(1, matrixPtr[x][y]); //1 = "pin one" on Raspi --> y-"data" pin
-			yClock();
+//scans downward, across screen ONE FULL TIME.
+void printScreen(bool (**matrix)){	
+  //making assumption of matrix form matrix[x][y]
+  for(int y = ARRAY_HEIGHT-1; y >= 0; y--) { 
+		for(int x = ARRAY_WIDTH-1;x >= 0; x--){
+			digitalWrite(ROW, (matrix[y][x])); //1 = "pin one" on Raspi --> x-"data" pin
+			xClock();
 		}
-		if(x==0){ 
+		if(y==0){ 
 			for (int i = 0; i<2; i++){
-				digitalWrite(0, HIGH); //0 = "pin zero" on RasPi --> x-"data" pin
+				digitalWrite(COL, HIGH); // y-"data" pin
 				xClock();
-				digitalWrite(0, LOW);
+				digitalWrite(COL, LOW);
 				for (int z=0;z<8;z++) xClock();
 			}
-			digitalWrite(0, HIGH);
+			digitalWrite(COL, HIGH);
 			xClock();
-			digitalWrite(0, LOW);
+			digitalWrite(COL, LOW);
 		}
-		else xClock(); //shifts the data over to make sure the proper column is lit
+		else yClock(); //shifts the data over to make sure the proper column is lit
 		outputToScreen();
-		usleep(520); // leaves the screen on for a while before the next line is lit: 520us = ~120fps for 48x64 matrix 
-	}
-return;
+		// leaves the screen on for a while before the next line is lit = ~60fps
+    usleep(520); 
+  }
+	return;
 }
 
-void *printScreenImplement(void *vptr_value){//matrixPtr points to a bool 8x8 2-d array.
+//matrix points to a bool 2-d array.
+void *printScreenImplement(void *vptr_value) {
 	wiringPiSetup();
 	for (int i = 0; i<=4; i++){
 	pinMode(i, OUTPUT);
 	}
-	bool matrixPtr[48][64];
-	matrixPtr[0][0] = (bool*) vptr_value;
+	bool (**matrix) = (bool (**)) vptr_value;
 	flushAllRegisters(); 
 	while(1) {
-		printScreen(matrixPtr);
+		printScreen(matrix);
 	}
 }
 
-int main(bool matrixPtr){//matrixPtr points to a bool 64x48 2-d array. Points containing true interpreted on, false is off.
+int main(bool (**matrix)){//matrixPtr points to a bool 2-d array. Points containing true interpreted on, false is off.
 	pthread_t tid;
-	pthread_create(&tid, NULL, printScreenImplement, (void *) matrixPtr);
+	pthread_create(&tid, NULL, printScreenImplement, (void *) matrix);
 	return tid;
 }
