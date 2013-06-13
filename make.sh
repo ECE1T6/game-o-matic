646cee57ad891eea5c94576344e08fee65ab9623
rbracken <ryan.brackenbury@gmail.com>
2013-06-13 18:23:07 -0400

    Added compile file from master

---
+++ make.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Make certain that this file is placed IN the HIDEV directory.
+
+echo "Starting Compilation..."
+#hidev libraries
+cd hidev
+gcc -o pong pong.c -lm
+echo "Pong Compiled."
+gcc -o displayTest displayTest.c -lwiringPi -std=gnu99
+echo "displayTest Compiled."
+#lowdev libraries
+
+cd ../lodev
+
+gcc -o flushScreen flushScreen.c -lwiringPi
+echo "flushScreen Compiled."
+gcc -o printScreen printScreen.c -lwiringPi -std=gnu99
+echo "printScreen Compiled."
+echo "...Finished."
+exit 1
+
