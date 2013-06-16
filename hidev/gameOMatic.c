067bdcbc96c0b5f4a39ad3f7d1307d2113eeed2e
Ryan <ryan.brackenbury@gmail.com>
2013-06-15 23:37:44 -0400

    Added the gameOMatic wrapper function -- this will call all the necessary side functions/organize the menu.

---
+++ hidev/gameOMatic.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include "hidev.h" //custom library
+#include "lodev.h" //custom library
+
+//###### GLOBAL DEFINITIONS ####
+#define _GNU_SOURCE
+#define ARRAY_HEIGHT  48 //change these to the matrix size
+#define ARRAY_WIDTH  64
+#define TWEET_FILE /home/pi/tweets.txt //change this to the file where tweets are being stored
+
+void main(void){
+
+//### BEGINNING OF INITIALIZATION
+//system call to start the twitter getting program
+	system("java getTweets -Xms10M -Xmx40M -jar /home/pi/getTweets.jar file=TWEET_FILE"); 
+
+//malloc the display array
+	bool **screen;
+		screen = (bool**) malloc(ARRAY_HEIGHT*sizeof(bool*));
+		for  (int i=0; i<ARRAY_HEIGHT; i++){
+			screen[i] = (bool*) malloc (ARRAY_WIDTH*sizeof(bool));
+		}
+		for (int i=0; i<ARRAY_HEIGHT; i++){
+			for (int j=0; j<ARRAY_WIDTH; j++){
+				screen[i][j] = false;
+			}
+		}
+
+//spawn thread for running the display
+	pthread_t screenTid = printScreen(screen);
+
+//malloc controller data struct
+	struct ctlData {
+		int vert1;
+		int hor1;
+		int vert2;
+		int hor2;
+		bool buttonHit1;
+		bool buttonHit2;
+	};
+
+	struct ctlData *ctl;
+	ctl = (struct *ctlData) malloc(sizeof(struct ctlData));
+
+//spawn thread for getting controller data
+
+	pthread_t ctlTid = controller(ctl);
+
+//spawn thread to check 'reset' button
+	bool *reset;
+	reset = (bool*) malloc(sizeof(bool*));
+	*reset = false;
+	pthread_t chkTid = restChkr(reset);
+
+//### END INITIALIZATION ###
+
+while(reset == false){
+//display menu for games selection and tweets
+		while(ctl.buttonHit1 != true && ctl.buttonHit2 != true){
+			//display dem tweets and dem menus
+		}
+//when a selection is made: (BEWARE! pseudo-code galore!)
+		stopDisplayingTweets();
+		stopDisplayingMenu();
+		blankArray(screen, ARRAY_HEIGHT, ARRAY_WIDTH);
+		if (selection == 1) //start pong
+			pong(screen, ctl); 
+		else if (selection == 2) //start snake
+			snake(screen, ctl);
+		blankArray(screen, ARRAY_HEIGHT, ARRAY_WIDTH);
+	}
+//begin shutdown routine
+	//unmalloc all the things that were malloc'd!
+	//exit or kill all threads
+	pthread_join(ctlTid, NULL);
+	pthread_join(chkTid, NULL);
+	pthread_join(screenTid, NULL);
+	system("sudo restart"); // or simply just restart the program?
+return;
+}
+
