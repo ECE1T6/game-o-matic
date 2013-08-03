a2b282abe9c3612ba5c923269bf6de19d56e072b
Amer Hesson <amer.hesson@mail.utoronto.ca>
2013-08-03 15:01:43 -0400

    Updated install.sh with latest refactor.

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 #Installs pong to the init.d directory, to be started at boot.
 #NEEDS TO BE RUN WITH SUDO PRIVELEDGES
 
-cd hidev
-sudo mv displayTest /etc/init.d/
-sudo chmod +x /etc/init.d/displayTest
-sudo update-rc.d displayTest defaults 
+cd lodev
+sudo mv run /etc/init.d/gameomatic
+sudo chmod +x /etc/init.d/gameomatic
+sudo update-rc.d gameomatic defaults 
 
 echo "Installed."
-exit 1
+exit 0
