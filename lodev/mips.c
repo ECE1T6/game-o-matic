0f8e48a81cf8408368d76b2e3e4edce5cbeb6a13
Ryan <ryan.brackenbury@gmail.com>
2013-09-02 05:19:57 -0400

    Added CPU busy-loop calibrator

---
+++ lodev/mips.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void *mips(void *args) {
+  unsigned long long *mips = (unsigned long long*) args;
+  while (1) {
+    *mips += 1;
+  }
+
+  return NULL;
+}
+
+
+int main (void){
+	unsigned long long armmips = 0;
+	pthread_t tid;
+	pthread_create(&tid, NULL, mips, &armmips);
+	usleep(1000000);
+	pthread_cancel(tid);
+	armmips *= 0.00001;
+	printf("Armmips: %llu\n", armmips); //uncomment to print results to terminal.
+	return (int) armmips;
+}
