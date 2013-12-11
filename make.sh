1c78b7766a143d85cbbd844a487fe124db2f9395
Ryan B <ryan.brackenbury@gmail.com>
2013-12-11 15:29:55 -0500

    Update make.sh

--- make.sh
+++ make.sh
@@ -6,12 +6,22 @@ echo "Starting Compilation..."
 cd hidev
 gcc gameomatic.c helpers.c pong.c tetrisGame.c snakeTwoPlayer.c snakeSinglePlayer.c -o gameomatic -lm -std=gnu99
 echo "...Game-O-Matic Compiled."
+GCC = $?
+if [ $RET1 -ne 0 ]; then #compilation failed for some reason
+  echo "Hidev Compilation FAILED"
+  exit 1;
+fi
 
 #lodev libraries
 cd ../lodev
 
 echo "Compiling low-level libraries..."
 gcc -Wall -pthread displayTest.c lodevInit.c register.c render.c control.c -o displayTest -lwiringPi -std=gnu99
-echo "Compiled lodev libraries; executable as 'run'."
-echo "...Finished."
-exit 0
+GCC = $?
+if [ $GCC -eq 0 ]; then #sucessful compilation
+  echo "Compiled lodev libraries; executable as 'run'."
+  echo "...Finished Succesfully."
+else #compilation of lodev failed!
+  echo "Lodev Compilation FAILED"
+fi
+exit $GCC
