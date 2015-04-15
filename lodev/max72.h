d90c3625ee37ba7bcddf7ee6a48c5e1213e5ccfe
Ryan B <ryan.brackenbury@gmail.com>
2015-04-14 22:48:41 -0400

    Create max72.c
    THe header file!!

---
+++ lodev/max72.h
@@ -0,0 +1,20 @@
+//Header file for max72.c
+
+// Definitions
+byte max7219_reg_noop        = 0x00;
+byte max7219_reg_digit0      = 0x01;
+byte max7219_reg_digit1      = 0x02;
+byte max7219_reg_digit2      = 0x03;
+byte max7219_reg_digit3      = 0x04;
+byte max7219_reg_digit4      = 0x05;
+byte max7219_reg_digit5      = 0x06;
+byte max7219_reg_digit6      = 0x07;
+byte max7219_reg_digit7      = 0x08;
+byte max7219_reg_decodeMode  = 0x09;
+byte max7219_reg_intensity   = 0x0a;
+byte max7219_reg_scanLimit   = 0x0b;
+byte max7219_reg_shutdown    = 0x0c;
+byte max7219_reg_displayTest = 0x0f;
+
+
+//Functions from library
